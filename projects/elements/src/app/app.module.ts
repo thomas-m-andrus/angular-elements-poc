cff32a86dccb569e5a9cbd1bec1771a60322f179
Thomas Andrus <thomasmandrus@gmail.com>
2023-06-30 19:52:49 -0500

    add package

--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AwesomeComponentComponent } from './awesome-component/awesome-component.component';
-import { Injector } from '@angular/core';
-import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AwesomeComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
@@ -17,5 +13,4 @@ import { createCustomElement } from '@angular/elements';
   providers: [],
   bootstrap: [AppComponent]
 })
-
-export class AppModule {}
+export class AppModule { }
